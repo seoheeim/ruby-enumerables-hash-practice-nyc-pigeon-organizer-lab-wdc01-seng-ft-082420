2464b5c50cbb0e0e9eeecc483b42b08b69dbe872
Seohee  Im <seoheeim@vt.edu>
2020-07-28 18:58:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,8 +1,17 @@
 require 'pry'
 
 def nyc_pigeon_organizer(data)
-  final = data.each_with_object({}) do |(key, value), final|
+  final_result = data.each_with_object({}) do |(key, value), final_array|
      value.each do |inner_key, names|
        names.each do |name|
-         binding.pry
+         if !final_array[name]
+           final_array[name] = {}
+         end
+         if !final_array[name][key]
+           !final_array[name][key] = []
+         end
+         final_array[name][key].push(inner_key.to_s)
+       end
+     end
+   end
 end
