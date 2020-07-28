06f7544cbf8d9c4f3fa5dfc9023088aa54d0c3ae
Seohee  Im <seoheeim@vt.edu>
2020-07-28 18:52:03 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,5 +2,7 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   final = data.each_with_object({}) do |(key, value), final|
-     bind.pry
+     value.each do |inner_key, names|
+       names.each do |name|
+         biding.pry
 end
